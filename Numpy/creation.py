d747a02902486315209c5b5dbc3659c62032f57f
rahullllll03 <rahulghorpade0707@gmail.com>
2026-02-10 13:29:22 +0530

    Add Numpy and Pandas data visualization notebook and dataset

---
+++ Numpy/creation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([[10, 20, 30, 40], [20, 456, 95, 555], [423, 555, 53, 34]])
+# , [20, 456, 95, 555], [423, 555, 53, 34]
+print(arr[:])
+print(np.shape(arr))
+print(np.size(arr))
+print(np.ndim(arr))
+print(arr.dtype)
+# print(type(arr))
